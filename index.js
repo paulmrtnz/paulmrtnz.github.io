1161a343727a7f406978090c13d253bb2a6e10c8
Paul Martinez <74772848+paulmrtnz@users.noreply.github.com>
2025-01-21 13:10:04 +0100

    Commit initial, contenu principal
    Ajout de la structure basique.
    Ajout des sections "formation" et "projets"
    Ajout du processus de remplissage depuis le fichier "data.js" pour la section "projets"

---
+++ index.js
@@ -0,0 +1,56 @@
+import {
+    projets
+} from './user-data/data.js';
+
+function populateProjets(items, id) {
+    const target = document.getElementById(id);
+
+    for (let i = 0; i < items.length ; i++) {
+        const projetItem = document.createElement("div");
+        projetItem.className = "blog-card border border-slate-300 rounded-md px-2 py-1 md:px-4 md:py-2 flex flex-col space-y-1 shadow";
+    
+        // Créer le titre
+        const projetTitre = document.createElement("h5");
+        projetTitre.className = "text-lg font-semibold";
+        projetTitre.innerText = items[i].titre;
+        projetItem.appendChild(projetTitre);
+
+        // Créer la date
+        const projetDate = document.createElement("p");
+        projetDate.className = "subtext";
+        projetDate.innerText = items[i].date;
+        projetItem.appendChild(projetDate);
+
+        // Créer la description
+        const projetDescription = document.createElement("p");
+        projetDescription.className = "";
+        projetDescription.innerText = items[i].description;
+        projetItem.appendChild(projetDescription);
+
+        // Créer les tags
+        const projetTagsConteneur = document.createElement("div");
+        projetTagsConteneur.className = "flex flex-wrap space-x-2 projet-tags";
+        for (let j = 0; j < items[i].tags.length; j++) {
+            const projetTag = document.createElement("span");
+            projetTag.className = "projet-tag bg-indigo-200";
+            projetTag.innerHTML = items[i].tags[j];
+            projetTagsConteneur.appendChild(projetTag);
+        }
+        projetItem.appendChild(projetTagsConteneur);
+
+        // Créer les liens
+        const projetLiensConteneur = document.createElement("div");
+        projetLiensConteneur.className = "flex flex-wrap space-x-2 projet-liens";
+        for (let k = 0; k < items[i].liens.length; k++) {
+            const projetLien = document.createElement("a");
+            projetLien.href = items[i].liens[k].url;
+            projetLien.target = "_blank";
+            projetLiensConteneur.appendChild(projetLien);
+        }
+        projetItem.appendChild(projetLiensConteneur);
+        
+        target.appendChild(projetItem);
+    }
+}
+
+populateProjets(projets, "projets");
---
+++ user-data/data.js
@@ -0,0 +1,11 @@
+export const projets = [
+    { 
+        titre: "Serre connectée",
+        date: "juin 2022",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec pur",
+        tags: ["1<sup>ère</sup> année INSA", "En équipe", "R&D", "Programmation", "IoT"],
+        liens: [
+            { type: "github", url: "#", label: "Github" },
+        ]
+    }
+];
